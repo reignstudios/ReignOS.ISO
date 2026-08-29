dfe1bfdc584ea998c36a43b0d8e872ef7af0b3a9
Andrew <zezba9000@gmail.com>
2026-08-28 17:50:44 -0700

    Improve installer boot to auto try other options

--- airootfs/home/gamer/install.sh
+++ airootfs/home/gamer/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Giving mesa a few seconds..."
+sleep 5
+
 # args
 USE_WESTON=false
 USE_CAGE=false
@@ -23,6 +26,11 @@ for arg in "$@"; do
  fi
 done
 
+# default arg if none set
+if [ "$USE_WESTON" != "true" ] && [ "$USE_CAGE" != "true" ] && [ "$USE_LABWC" != "true" ] && [ "$USE_X11" != "true" ]; then
+ USE_WESTON=true
+fi
+
 # config installer
 echo "Starting ReignOS.Installer..."
 cd /home/gamer/ReignOS/Installer/ReignOS.Installer/bin/Release/net10.0/linux-x64/publish/
@@ -30,21 +38,31 @@ chmod +x ./ReignOS.Installer
 
 if [ "$USE_WESTON" = "true" ]; then
  weston --shell=kiosk-shell.so --xwayland -- ./ReignOS.Installer -weston
+
+ # relaunch
+ echo "Hit Ctrl+C if you don't want to relaunch installer..."
+ sleep 5
+ /home/gamer/install.sh -cage
 fi
 
 if [ "$USE_CAGE" = "true" ]; then
  cage -d -s -- ./ReignOS.Installer -cage
+
+ # relaunch
+ echo "Hit Ctrl+C if you don't want to relaunch installer..."
+ sleep 5
+ /home/gamer/install.sh -labwc
 fi
 
 if [ "$USE_LABWC" = "true" ]; then
  labwc --startup "./ReignOS.Installer -labwc"
+
+  # relaunch
+ echo "Hit Ctrl+C if you don't want to relaunch installer..."
+ sleep 5
+ /home/gamer/install.sh -x11
 fi
 
 if [ "$USE_X11" = "true" ]; then
  startx
 fi
-
-# relaunch
-echo "Hit Ctrl+C if you don't want to relaunch installer..."
-sleep 5
-/home/gamer/install.sh $@
